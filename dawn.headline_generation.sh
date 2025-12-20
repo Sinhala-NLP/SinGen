e85d80aa5d6f5b5a4b2dd1976708db3379131e1e
TharinduDR <rhtdranasinghe@gmail.com>
2025-12-20 16:26:53 +0000

    001: Initial commit

--- dawn.headline_generation.sh
+++ dawn.headline_generation.sh
@@ -1,21 +1,19 @@
 #!/bin/bash -l
-#SBATCH --job-name=headline_generation
-#SBATCH --account=dn-rana1
+#SBATCH --job-name=headline_generation-gpu
+#SBATCH --account=AIRR-P39-DAWN-GPU
 #SBATCH --partition=pvc9
-#SBATCH -n 1   # Number of tasks (usually number of MPI ranks)
-#SBATCH -c 24  # Number of cores per task
+#SBATCH --nodes=1               # number of nodes
 #SBATCH --gres=gpu:1 # Number of requested GPUs per node
 #SBATCH --time=06:00:00              # total run time limit (HH:MM:SS)
 
 module purge
-module load rhel9/default-dawn
+module load rhel8/default-dawn
 module load intelpython-conda
 
 conda activate ~/rds/conda_envs/llm_exp
 
-export HF_HOME=/home/dn-rana1/rds/hf_home
+export HF_HOME=/home/dn-rana1/rds/hf_home/
+export HF_TOKEN=""
 
-
-huggingface-cli login --token
 
 python -m headline_generation.hf_llm --model_id='meta-llama/Llama-3.3-70B-Instruct' --query_type='zero-shot-si'
